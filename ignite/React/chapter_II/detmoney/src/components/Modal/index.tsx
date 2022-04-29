543ce5dc3b3b7843fbf716eaac69caf4ba9b9076
Gabriel0109 <gabrielglfg@outlook.com>
2022-04-29 18:56:16 -0300

    adding transaction to table

--- ignite/React/chapter_II/detmoney/src/components/Modal/index.tsx
+++ ignite/React/chapter_II/detmoney/src/components/Modal/index.tsx
@@ -18,16 +18,21 @@ export function NewTransactionModal({ onRequestClose, isOpen }: NewTransactionMo
     const [category, setCategory] = useState('')
     const [type, setType] = useState('deposit')
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault()
 
-        createTransaction({
+        await createTransaction({
             title,
             amount,
             category,
             type,
 
         })
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
     }
 
     return (
