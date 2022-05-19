463876845650b822f65beb880b9e9df6c6652e6e
Gabriel0109 <gabrielglfg@outlook.com>
2022-05-19 10:35:22 -0300

    subscribe btn css

---
+++ ignite/React/chapter_III/ignews/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.css';
+
+
+
+export function SubscribeButton(){
+    return(
+        <button
+            type="button"
+            className={styles.subscribeButton}
+        >
+
+        </button>
+    )
+}
