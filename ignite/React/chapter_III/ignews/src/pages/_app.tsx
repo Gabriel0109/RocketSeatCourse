b7b454949ef9ba8f2214bcdc9e3889f304c78ec6
gabriel0109 <gabrielglfg@outlook.com>
2022-05-23 14:57:02 -0300

    next-auth login btn functional

--- ignite/React/chapter_III/ignews/src/pages/_app.tsx
+++ ignite/React/chapter_III/ignews/src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { SessionProvider as NextAuthProviders } from 'next-auth/react'
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps } : AppProps) {
 
   return( 
-    <>
+    <NextAuthProviders session={pageProps.session}>
     <Header />
   <Component {...pageProps} />
-    </>
+    </NextAuthProviders>
   )
 
 }
