1be9c16f61cadcff89d65751056ad17ba5888b90
Gabriel Guimarães <82231197+Gabriel0109@users.noreply.github.com>
2022-05-23 21:58:10 -0300

    passing user to db

--- ignite/React/chapter_III/ignews/src/pages/api/auth/[...nextauth].ts
+++ ignite/React/chapter_III/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
+
+import { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-
+import {fauna } from '../../../services/fauna'
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -13,4 +16,21 @@ export default NextAuth({
      }
     }),
   ],
+  callbacks:{
+    async signIn({ user, account, profile}){
+      const { email } = user
+try{
+
+  await fauna.query(
+    q.Create(
+      q.Collection('users'),
+      {data: {email}}
+    )
+  )
+  return true
+} catch{
+    return false
+}
+    }
+  }
 })
