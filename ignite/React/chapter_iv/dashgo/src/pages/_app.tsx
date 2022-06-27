45c2da5097d0756225aa443d12c35ad2f22da948
gabriel0109 <gabrielglfg@outlook.com>
2022-06-27 14:54:14 -0300

    style config theme from extendTheme  chakra

--- ignite/React/chapter_iv/dashgo/src/pages/_app.tsx
+++ ignite/React/chapter_iv/dashgo/src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import {AppProps} from 'next/app'
-import Head from 'next/head'
+import {ChakraProvider} from '@chakra-ui/react'
+import { theme } from '../styles/theme'
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+     <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+     </ChakraProvider>
+     )
 }
+export default MyApp
