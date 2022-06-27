a3788664fdfc107e8e8bfcfeda45dd21d593243c
gabriel0109 <gabrielglfg@outlook.com>
2022-06-27 16:29:13 -0300

    login page

--- ignite/React/chapter_iv/dashgo/src/pages/index.tsx
+++ ignite/React/chapter_iv/dashgo/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Input } from '@chakra-ui/react'
+import { Button, Flex, Input, Stack } from '@chakra-ui/react'
 export default function Home() {
   return (
     <Flex
@@ -11,14 +11,39 @@ export default function Home() {
         width="100%"
         maxWidth={360}
         bg="gray.800"
-        p={8}
+        p="8"
         flexDir="column"
         borderRadius={8}
-        >
-asd
-
-    <Input name='email' type="email" />
-    <Input name='password' type="password"/>
+      >
+        {/* EMAIL */}
+  <Stack spacing="4">
+          <Input
+            name='email'
+            type="email"
+            focusBorderColor='pink.500'
+            bgColor="gray.900"
+            variant="filled"
+            _hover={{
+              bgColor: 'gray.900'
+            }}
+            size="lg"
+          />
+          {/* PASSWORD */}
+          <Input
+            name='password'
+            type="password"
+            focusBorderColor='pink.500'
+            bgColor="gray.900"
+            variant="filled"
+            _hover={{
+              bgColor: 'gray.900'
+            }}
+            size="lg"
+          />
+  </Stack>
+        <Button type="submit" mt="6" colorScheme="pink">
+          Entrar
+        </Button>
       </Flex>
     </Flex>
   )
