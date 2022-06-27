ea307c40c7816ca44f5291ea50fea970232396fe
gabriel0109 <gabrielglfg@outlook.com>
2022-06-27 15:36:23 -0300

    login page

--- ignite/React/chapter_iv/dashgo/src/pages/index.tsx
+++ ignite/React/chapter_iv/dashgo/src/pages/index.tsx
@@ -1,6 +1,25 @@
-
+import { Flex, Input } from '@chakra-ui/react'
 export default function Home() {
   return (
-    <h1>DAshgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p={8}
+        flexDir="column"
+        borderRadius={8}
+        >
+asd
+
+    <Input name='email' type="email" />
+    <Input name='password' type="password"/>
+      </Flex>
+    </Flex>
   )
 }
