70c952ca8604b05cc9b6ecf766a63217f71eb0a6
gabriel0109 <gabrielglfg@outlook.com>
2022-06-30 14:06:03 -0300

    dynamic post page

--- ignite/React/chapter_III/ignews/src/pages/posts/index.tsx
+++ ignite/React/chapter_III/ignews/src/pages/posts/index.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic'
 import styles from './styles.module.scss'
 import { RichText } from 'prismic-dom'
+import Link from 'next/link'
 type Post = {
     slug: string;
     title: string;
@@ -27,11 +28,13 @@ export default function Posts({posts}){
             <div className={styles.posts}>
                 {posts.map(post => (
  // eslint-disable-next-line react/jsx-key
-                <a key={post.slug} href="">
-                <time>{post.data}</time>
-                <strong>{post.title}</strong>
-                <p>{post.excerpt}</p>
-                </a>
+                <Link href={`posts/${post.slug}`}>
+                    <a key={post.slug} >
+                    <time>{post.data}</time>
+                    <strong>{post.title}</strong>
+                    <p>{post.excerpt}</p>
+                    </a>
+                </Link>
                 ))}
                
             </div>
